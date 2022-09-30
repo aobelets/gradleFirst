73767c70cf12e0c7129914acd6bec388540df30e
aobelets <aobelec3@gmail.com>
2022-10-01 00:17:40 +0200

    commit 4

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,15 +1,7 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.By;
-//import org.junit.jupiter.api.Assertions.*;
-import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selectors.byName;
-//import org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.openqa.selenium.support.ui.ExpectedConditions.invisibilityOfElementLocated;
-
 
 public class Program {
 
@@ -18,35 +10,17 @@ public class Program {
         Configuration.holdBrowserOpen = true;
 
         Selenide.open("https://www.i.ua");
-        Selenide.Wait().until(invisibilityOfElementLocated(By.id("magic-id")));
         Selenide.$(byName("login")).setValue("andreyobelec").pressEnter();
-        Selenide.$(byName("pass")).setValue("qwerty").pressEnter();
+        Selenide.$(byName("pass")).setValue("d7m2y1982").pressEnter();
 
-        Selenide.$(byLinkText("Помощь")).click();
-        String currentUrl = WebDriverRunner.getWebDriver().getCurrentUrl();
-        //webdriver().shouldHave(url("https://help.i.ua/section/11/"));
+        String[] array = new String[40];
 
-        //assertEquals("https://help.i.ua/section/11/", currentUrl);
-        if (currentUrl == "https://help.i.ua/section/11/"){
-            System.out.println("ok");
-        } else{
-            System.out.println("wrong");
+        for (int i = 20; i < 40; i++) {
+            String sel =  "#mesgList > form > div:nth-child(" + i + ") > a > span.frm";
+            array[i]=Selenide.$(sel).getText();
+            System.out.println(array[i]);
         }
-        System.out.println(currentUrl);
-
-
-
-
-
-        //Selenide.$(byTitle("Вхід на пошту")).click();
-
-        // System.out.println("=======Running @After for JUnit 4=======");
-        //Selenide.open("https://www.google.com", "i.ua","andreyobelec", "qwerty");
-
-
-        //FirefoxDriverManager.getInstance().setup();
-        //System.setProperty("webdriver.gecko.driver", "/home/andrew/snap/firefox/common/tmp/geckodriver");
-        //FirefoxDriver driver = new FirefoxDriver();
-        //driver.get("https://www.google.com");
+        System.out.println(array);
+        Selenide.closeWindow();
     }
 }
